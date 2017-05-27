45f63754c5aac9a2390271eb76fb5e05d2c44353
netyouli <712641411@qq.com>
2017-05-27 11:29:23 +0800

    兼容Mac ox tv os

--- SexyJsonKit/SexyJson.swift
+++ SexyJsonKit/SexyJson.swift
@@ -25,7 +25,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import UIKit
+import Foundation
 
 fileprivate extension _SexyJsonBase {
     //// parser json keypath value
--- SexyJsonKit/SexyJsonProtocol.swift
+++ SexyJsonKit/SexyJsonProtocol.swift
@@ -24,7 +24,11 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import UIKit
+#if os(iOS) || os(tvOS)
+    import UIKit
+#else
+    import AppKit
+#endif
 
 //MARK: - Public protocol -
 
